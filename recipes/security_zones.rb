ac6591a33accd7c020ad35944bccd123ee57017a
Mike Hemesath <mike.hemesath@gmail.com>
2015-03-29 13:50:54 -0500

    Update security_zones.rb

--- recipes/security_zones.rb
+++ recipes/security_zones.rb
@@ -1,5 +1,5 @@
 # https://support.microsoft.com/en-us/kb/182569
-KEY = 'HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Zones'
+KEY = 'HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Zones'
 
 if platform?('windows')
   security_zones =  node['ie']['zone']
